80d5f8e184a36394294ff261958afbe41f433a2e
Olena Mokina <olena.mokina@epam.com>
2021-10-27 16:59:12 +0300

    naming correction

--- src/test/java/DateTimeCheck.java
+++ src/test/java/DateTimeCheck.java
@@ -57,5 +57,4 @@ public class DateTimeCheck {
         return previousDate.before(nextDate);
     }
 
-
 }
--- src/test/java/SoftAssertJUnit.java
+++ src/test/java/SoftAssertJUnit.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class softAssertJUnit {
+public class SoftAssertJUnit {
 
     @Test
     public void test001() {
